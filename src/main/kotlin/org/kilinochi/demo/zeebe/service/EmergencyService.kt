501b3795905dc5de6391ca1317110faa863a3831
arman.shamenov <AShamenov@alfabank.ru>
2021-02-25 13:12:06 +0500

    first

--- src/main/kotlin/org/kilinochi/demo/zeebe/service/EmergencyService.kt
+++ src/main/kotlin/org/kilinochi/demo/zeebe/service/EmergencyService.kt
@@ -1,6 +1,7 @@
 package org.kilinochi.demo.zeebe.service
 
 import io.zeebe.spring.client.ZeebeClientLifecycle
+import java.util.UUID
 import org.kilinochi.demo.zeebe.controller.dto.EmergencyReasonDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,7 +14,12 @@ class EmergencyService(
     fun startEvent(reason: EmergencyReasonDto) {
         val emergencyReason = reason.emergencyReason
         val event = zeebeClient.newCreateInstanceCommand().bpmnProcessId("EMERGENCY_PROCESS")
-            .latestVersion().variables("{\"emergencyReason\": \"$emergencyReason\"}").send().join()
+            .latestVersion().variables("{\n" +
+                    "  \"emergencyReason\": {\n" +
+                    "    \"status\": \"$emergencyReason\",\n" +
+                    "    \"id\": ${UUID.randomUUID()}\n" +
+                    "  }\n" +
+                    "}").send().join()
         logger.info(
             "started instance for workflowKey='{}', bpmnProcessId='{}', version='{}' with workflowInstanceKey='{}'",
             event.workflowKey, event.bpmnProcessId, event.version, event.workflowInstanceKey
